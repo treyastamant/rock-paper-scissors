82c23c8c989dd6abb9a2af14160ad8bff3ef23c8
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-04-19 21:40:53 -0600

    Add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,17 @@
+// getComputerChoice to randomly return 'Rock' 'Paper' or 'Scissors'
+
+function getComputerChoice() {
+    let choice = ["Rock", "Paper", "Scissors"];
+    return choice[Math.floor(Math.random() * 3)];
+}
+
+console.log(getComputerChoice());
+
+
+// function round(playerSelection, computerSelection) return string declaring winner
+    // make case insensitive
+
+
+// function game() loop 5 rounds, keeping track of score
+
+// prompt() to get input from user
