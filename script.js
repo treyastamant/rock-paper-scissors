88e9c889de5313e59cc80cba9f4f3a375c2d283c
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-04-20 08:52:39 -0600

    Add scoring and prompt

--- script.js
+++ script.js
@@ -1,51 +1,47 @@
-// getComputerChoice to randomly return 'Rock' 'Paper' or 'Scissors'
-
 function getComputerChoice() {
     let choice = ["Rock", "Paper", "Scissors"];
     return choice[Math.floor(Math.random() * 3)];
 }
+let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
 
-const playerSelection = "SCIssors";
-
-// function round(playerSelection, computerSelection) return string declaring winner
-    // make case insensitive
 
 function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("Rock, Paper, or Scissors?");
     let choices = playerSelection.toLowerCase() + computerSelection.toLowerCase();
     let message;
 
     if ((choices === "rockscissors") || (choices === "scissorspaper") || (choices === "paperrock")) {
         message = "You win!";
+        playerScore++;
     } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-        message = "It's a tie!";
+        message = "It's a tie.";
     } else {
         message =  "You lose!";
+        computerScore++;
     }
     return message;
 }
-
-// function game() {
-//     let round1 = playRound(playerSelection, computerSelection);
-//     let round2 = playRound(playerSelection, computerSelection);
-//     console.log(round1);
-//     console.log(round2);
-// }
 
 function game() {
     for (let round = 1; round < 6; round++) {
         let roundResults = playRound(playerSelection, getComputerChoice());
         console.log("Round " + round + ": " + roundResults);
     }
+    let endGameMessage
+    if (playerScore > computerScore) {
+        endGameMessage = "** You won the game! **";
+    } else if (playerScore < computerScore) {
+        endGameMessage = "** You lost the game! **";
+    } else {
+        endGameMessage = "** It was a tie game. **";
+    }
+    console.log(endGameMessage);
 }
 
 console.log(game());
 
+// TODO make variables for player and computer with 0 in playRound function 
+// add ++ to winner of each game in game function
 
-
-
-// console.log(playRound(playerSelection, computerSelection));
-
-
-// function game() loop 5 rounds, keeping track of score
-
-// prompt() to get input from user
