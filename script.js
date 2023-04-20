bf65a4f3a581d5f2b01bec57a3294b55b7f7faa8
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-04-19 22:49:37 -0600

    wip

--- script.js
+++ script.js
@@ -5,11 +5,45 @@ function getComputerChoice() {
     return choice[Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice());
-
+const playerSelection = "SCIssors";
 
 // function round(playerSelection, computerSelection) return string declaring winner
     // make case insensitive
+
+function playRound(playerSelection, computerSelection) {
+    let choices = playerSelection.toLowerCase() + computerSelection.toLowerCase();
+    let message;
+
+    if ((choices === "rockscissors") || (choices === "scissorspaper") || (choices === "paperrock")) {
+        message = "You win!";
+    } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        message = "It's a tie!";
+    } else {
+        message =  "You lose!";
+    }
+    return message;
+}
+
+// function game() {
+//     let round1 = playRound(playerSelection, computerSelection);
+//     let round2 = playRound(playerSelection, computerSelection);
+//     console.log(round1);
+//     console.log(round2);
+// }
+
+function game() {
+    for (let round = 1; round < 6; round++) {
+        let roundResults = playRound(playerSelection, getComputerChoice());
+        console.log("Round " + round + ": " + roundResults);
+    }
+}
+
+console.log(game());
+
+
+
+
+// console.log(playRound(playerSelection, computerSelection));
 
 
 // function game() loop 5 rounds, keeping track of score
